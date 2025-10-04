89e2fb2d29132744c7a19539d4458e6e7c70525b
ewtmshu <55389723+ewtmshu@users.noreply.github.com>
2025-10-04 23:18:50 +0800

    Update index.tsx

--- pages/evidence/index.tsx
+++ pages/evidence/index.tsx
@@ -33,7 +33,7 @@ export default function Evidence({ items }: { items: Node[] }) {
 export async function getStaticProps() {
   try {
     type ListByCategoryResp = { posts: { nodes: Node[] } };
-    const data = await client.request<ListByCategoryResp>(queries.listByCategory, {
+    const data = (await client.request(queries.listByCategory, { cat: "evidence", first: 12 })) as any;
       cat: "evidence",
       first: 12,
     });
