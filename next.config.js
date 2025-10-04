0b5b0afcef8e173c4fc5bb789a465a6c5f276692
ewtmshu <55389723+ewtmshu@users.noreply.github.com>
2025-10-04 19:59:45 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cms.painphysicianwei.tw' },
+      { protocol: 'https', hostname: 'images.unsplash.com' }
+    ]
+  }
+};
 module.exports = nextConfig;
