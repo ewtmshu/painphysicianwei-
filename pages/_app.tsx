1b58e868369b39611e720c7fec078d8898ddf8aa
ewtmshu <55389723+ewtmshu@users.noreply.github.com>
2025-10-06 23:54:35 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,12 @@
+// pages/_app.tsx
 import type { AppProps } from "next/app";
-import "@/styles/globals.css";
-export default function App({ Component, pageProps }: AppProps) { return <Component {...pageProps} />; }
+import "../styles/globals.css";        // 第 4 步會建立
+import Layout from "../components/Layout";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
