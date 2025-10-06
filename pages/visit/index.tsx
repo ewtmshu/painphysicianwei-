f5ae11e20372747604ac69a2dfa56abdd4dcde54
ewtmshu <55389723+ewtmshu@users.noreply.github.com>
2025-10-06 12:27:23 +0800

    Create index.tsx

---
+++ pages/visit/index.tsx
@@ -0,0 +1,11 @@
+export default function VisitIndex() {
+  return (
+    <main className="container prose max-w-none">
+      <h1>就診資訊</h1>
+      <h3>土城康澤復健科診所（院長）</h3>
+      <p>新北市土城區中央路二段304號　電話：02-77287912</p>
+      <h3>龜山康澤復健科診所（指導院長）</h3>
+      <p>桃園市龜山區萬壽路二段1075號　電話：03-2717161</p>
+    </main>
+  );
+}
