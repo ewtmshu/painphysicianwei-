b634e971ad76027d39ce8207f3333d8fc7d22709
ewtmshu <55389723+ewtmshu@users.noreply.github.com>
2025-10-06 11:48:34 +0800

    Update doctor.tsx

--- pages/doctor.tsx
+++ pages/doctor.tsx
@@ -1,1 +1,13 @@
-
+export default function Doctor() {
+  return (
+    <main className="container prose max-w-none">
+      <h1>許哲維醫師</h1>
+      <p>復健醫學科、疼痛專科（CIPS）。</p>
+      <ul>
+        <li>Facebook：<a href="https://www.facebook.com/profile.php?id=100090615120394" target="_blank">許哲維醫師 Wei MD</a></li>
+        <li>Instagram：<a href="https://www.instagram.com/painphysicianforyou" target="_blank">@painphysicianforyou</a></li>
+        <li>YouTube：<a href="https://www.youtube.com/@復健專科_許哲維醫師" target="_blank">復健專科_許哲維醫師</a></li>
+      </ul>
+    </main>
+  );
+}
